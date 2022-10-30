b6909a08531d040ef5652578530988a2c0d90177
Mayank Saxena <saxenamayank0303@gmail.com>
2022-10-30 22:20:35 +0530

    Added Create and Update Function

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 /* Model for Course */
@@ -53,6 +56,57 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 	/* If course id matches, course will be returned */
 	for _, course := range courses {
 		if course.CourseId == courseId {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No course found for given ID")
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create course")
+	w.Header().Set("Content-Type", "application/json")
+
+	/* If body is empty */
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data")
+		return
+	}
+
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+
+	/* If course is empty */
+	if isEmpty(&course) {
+		json.NewEncoder(w).Encode("No data inside JSON")
+		return
+	}
+
+	/* Generate unique ID */
+	rand.Seed(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+
+	/* Add course to the slice */
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
+}
+
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update course")
+	w.Header().Set("Content-Type", "application/json")
+
+	/* Get the course id from the url */
+	params := mux.Vars(r)              /* Similar to r.URL.Query() */
+	courseId := params.Get("courseid") /* Get courseid */
+
+	/* If course id matches, course will be returned */
+	for index, course := range courses {
+		if course.CourseId == courseId {
+			courses = append(courses[:index], courses[index+1:]...)
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			course.CourseId = courseId
+			courses = append(courses, course)
 			json.NewEncoder(w).Encode(course)
 			return
 		}
